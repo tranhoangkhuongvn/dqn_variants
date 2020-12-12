3556d3d6f1c23fac6d04c63e601b76f33d9cea1a
Khuong Tran <tranhoangkhuongvn@gmail.com>
2020-12-12 21:27:05 +1100

    Update utils

--- visualise.py
+++ visualise.py
@@ -7,10 +7,12 @@ if __name__ == '__main__':
 	# read out npy files
 	stats = []
 	num_episodes = 2000
-	for i in range(2):
+	for i in range(1):
 		stat = Stats(2000)
 		with open('trial' + str(i) + '.npz', 'rb') as f:
 			data = np.load(f)
+			print('reward:', data['reward'])
+			print('step:', data['step'])
 			stat.episode_rewards = data['reward'] 
 			stat.episode_steps = data['step'] 
 		stats.append(stat)
@@ -21,4 +23,6 @@ if __name__ == '__main__':
 
 	ax2 = fig.add_subplot(122)
 	plot_steps(ax2, stats, 20)
+
+	plt.tight_layout()
 	plt.savefig('test.png')
